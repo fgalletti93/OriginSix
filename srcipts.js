6bb2cf3eb797917f6b3d468d185d0657c838df3c
Francesca Beatrice Galletti <fgalletti93@yahoo.com>
2021-07-19 14:41:33 -0300

    produto final NLW Together - Origin

--- srcipts.js
+++ srcipts.js
@@ -41,7 +41,13 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 
 })
 
@@ -73,3 +79,33 @@ window.addEventListener('scroll', function() {
         arrowUpButton.classList.remove('show')
     }
 })
+
+//Menu ativo conforme a seção vidível na pagina
+const sections = document.querySelectorAll('main section[id]')
+    
+function activateMenuAtCurrentSection () {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+    for( const section of sections ) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+        } else {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+     }
+}
+
+window.addEventListener('scroll', function (){
+    activateMenuAtCurrentSection()
+})
+
